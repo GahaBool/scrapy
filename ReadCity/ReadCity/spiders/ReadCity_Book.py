3c40466a810263b0f7e7548bb2d7a3d4e3693224
Egor Tirskih <egorhapik@mail.ru>
2024-01-22 23:10:10 +0800

    add new request

--- ReadCity/ReadCity/spiders/ReadCity_Book.py
+++ ReadCity/ReadCity/spiders/ReadCity_Book.py
@@ -7,5 +7,11 @@ class ReadcityBookSpider(scrapy.Spider):
     start_urls = ["https://www.chitai-gorod.ru/catalog/books-18030"]
 
     def parse(self, response):
+        for element in response.css('div.products-list'):
+            yield{
+                'Name' : element.css('article').extract(),
+                'Price' : element.css('article').extract()
+            }
 
-
+if __name__ == '__main__':
+    print()
